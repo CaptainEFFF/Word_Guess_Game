0fc45e66bbe511df625a56535fc28a9250208e17
CaptainEFFF <davidbanville4@gmail.com>
2019-04-10 23:59:43 -0400

    js almost workings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -28,10 +28,40 @@ console.log(underScore);
 console.log(rightAnswer);
 
 document.onkeyup = function(event) {
-    userGuess = event.key;
-    lives = lives - 1;
-    wrongGuess.push(userGuess);
-    console.log(lives);
-    console.log(wrongGuess);
-};
+    userGuess = event.key.toLowerCase();
+//     for(var j = 0; j< rightAnswer.length; j++){
     
+//         if (rightAnswer[j] === userGuess){
+//         rightGuess.push(rightAnswer[j]);
+//     }
+// }
+    
+    
+    for(var j = 0; j< rightAnswer.length; j++)
+    {
+        
+        if(rightAnswer[j] === userGuess)
+        {
+          rightGuess.push(rightAnswer[j]);
+          
+        }
+        
+        else if (wrongGuess.includes(userGuess)){
+           break;
+        }
+        else if (rightAnswer[j] !== userGuess)
+        {
+        wrongGuess.push(userGuess);
+        }
+    lives = lives - 1;
+    // wrongGuess.push(userGuess);
+    console.log(lives);
+    console.log("wrong " + wrongGuess);
+    console.log("right " + rightGuess);
+    };
+    
+
+ 
+
+
+}
