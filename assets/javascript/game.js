d85409208de13e31eb737b442ede78f22765cdd2
CaptainEFFF <davidbanville4@gmail.com>
2019-04-11 16:36:41 -0400

    js 80% done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,55 +13,66 @@ var names = [
 
 var wrongGuess = [];
 var rightGuess = [];
+var fullName = [];
 var rightAnswer = [];
 var userGuess = "";
-var underScore = []
+var underScore = [];
+var counter = 0;
 var lives = 12;
+
+// Random name selected
 var name = [names[Math.floor(Math.random() * names.length)]];
 console.log(name);
 
+// Fill arrays for underscores and chosen name
 for (var i = 0; i < name.length; i++){
     underScore[i] = "_";
-    rightAnswer.push(name.charAt(i));
+    fullName.push(name.charAt(i));
 }
+
+// Truncates name to exclude duplicate letters
+rightAnswer = [...new Set(fullName)];
+  
 console.log(underScore);
 console.log(rightAnswer);
 
+
 document.onkeyup = function(event) {
     userGuess = event.key.toLowerCase();
-//     for(var j = 0; j< rightAnswer.length; j++){
     
-//         if (rightAnswer[j] === userGuess){
-//         rightGuess.push(rightAnswer[j]);
-//     }
-// }
+    if (rightGuess.includes(userGuess)||wrongGuess.includes(userGuess)){
+        console.log("Already Guessed");
+    }
     
     
-    for(var j = 0; j< rightAnswer.length; j++)
-    {
+    else if (rightAnswer.includes(userGuess)){
+        rightGuess.push(userGuess);
+        counter++;
+        console.log(counter);
         
-        if(rightAnswer[j] === userGuess)
-        {
-          rightGuess.push(rightAnswer[j]);
-          
-        }
         
-        else if (wrongGuess.includes(userGuess)){
-           break;
-        }
-        else if (rightAnswer[j] !== userGuess)
-        {
+    }
+    else {
+        
         wrongGuess.push(userGuess);
-        }
-    lives = lives - 1;
-    // wrongGuess.push(userGuess);
+        lives = lives - 1;
+    }
+    
     console.log(lives);
     console.log("wrong " + wrongGuess);
     console.log("right " + rightGuess);
-    };
-    
+   
+    if (counter === rightAnswer.length){
+        console.log("You Win");
+    }
 
+    if (lives <= 0){
+        console.log("You Lose")
+    }
+}
  
 
+// need to push correct letters to underScore
+// need to employ DOM methods to update html/css
+// need to create html and css
 
-}
